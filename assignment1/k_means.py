650b850f6f07f474cbc46944e7a63336fd6e2d65
Hella Haanstra <hellahaanstra@gmail.com>
2017-04-12 14:40:36 +0200

    data inladen werkt nog niet maar zo kom ik er wel

--- assignment1/k_means.py
+++ assignment1/k_means.py
@@ -2,7 +2,35 @@ import csv
 from sklearn.cluster import KMeans
 
 def read_data(filename):
-	pass
+	data = {}
+	with open(filename, 'r') as data_file:
+		rdr = csv.reader(data_file, delimiter=',')
+		next(rdr, None)
+
+		next_date = ''
+		date_entries = []
+		day = {}
+
+		for i, row in rdr:
+			patient, date, variable, value, times = row
+
+			next_date = rdr[(i + 1) % len(rdr)][2]
+
+			if variable == 'mood':
+				continue
+
+			day['date'] = date
+			day[variable] = float(value)
+
+			if date != next_date:
+				date_entries.append(day)
+				day = {}
+
+	return date_entries
+
+
+	# fix for missing data
+	# http://stackoverflow.com/questions/35611465/python-scikit-learn-clustering-with-missing-data
 
 def init_k_means(init_data, new_data):
     '''
@@ -10,12 +38,14 @@ def init_k_means(init_data, new_data):
     centers of the initial data @init_data.
     '''
 
-    kmeans = KMeans(n_clusters=10).fit(init_data)
-    
-    helper_mean = array van lengte n_clusters shape: [0, 0]
+	no_clusters = 10
+
+    kmeans = KMeans(n_clusters=no_clusters).fit(init_data)
+
+    helper_mean = [[0,0] for y in range(no_clusters)]
 
     for i, val in enumerate(kmeans.labels_):
-    	cluster_mean[val][0] += init_data[i]['mood'] 
+    	cluster_mean[val][0] += init_data[i]['mood']
     	cluster_mean[val][1] += 1
 
    	cluster_mean = []
@@ -27,4 +57,3 @@ def init_k_means(init_data, new_data):
 
 def predict_kmeans(data, cluster_mean):
     return cluster_mean[kmeans.predict(new_data)]
-
