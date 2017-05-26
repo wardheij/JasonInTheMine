b539b10d4686cb49c00bb75b66d6297525aa4da5
Ward Heij <wardheij@gmail.com>
2017-05-26 15:58:55 +0200

    Evaluation script, minor changes

--- assignment2/train_net.m
+++ assignment2/train_net.m
@@ -1,4 +1,4 @@
-function net = train_net(input, target, layers, method)
+function [net, tr] = train_net(input, target, layers, method)
 
 
 if strcmp(method, 'classification')
@@ -7,15 +7,20 @@ if strcmp(method, 'classification')
     net = patternnet(layers, 'trainscg', 'crossentropy');
     
     % train network
-    [net,~] = train(net,input,target);
+    [net, tr] = train(net,input,target);
     
 elseif strcmp(method, 'regression')
     
     % set up net
     net = feedforwardnet(layers);
     
+    % Set up Division of Data for Training, Validation, Testing
+    net.divideParam.trainRatio = 1;
+    net.divideParam.valRatio = 0.1;
+    net.divideParam.testRatio = 0.1;
+    
     % train network
-    net = train(net,input, target);
+    [net, tr] = train(net,input, target);
     
 else
     error('Invalid method specified. Select either "classification" or "regression"')
