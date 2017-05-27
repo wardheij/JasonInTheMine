15edd21799141ae7d58bb42d7711c9abddabab50
Wouter Vrielink <wouter-vrielink@hotmail.com>
2017-05-27 11:11:41 +0200

    dit werkt als het goed is al

--- assignment2/processData.m
+++ assignment2/processData.m
@@ -15,19 +15,23 @@ end
 labels = data(1,:);
 data(1,:) = [];
 
-% % TODO: omzetten data, dit doet: remove date
+
+% TODO: process date
+
+
+
+% remove date
 data(:,2) = [];
 
 preprocessed_data = str2double(data);
 
 processed_data = [];
-% TODO: add Wouters code that converts data to useful variables
 
 % This order:
 varlist = ["prop_starrating", "prop_review_score", "prop_brand_bool", "prop_location_score1", "prop_location_score2", "promotion_flag", "srch_length_of_stay", "srch_booking_window", "srch_query_affinity_score", "orig_destination_distance"];
 
 for i = varlist
-    index = find(label, i);
+    index = find(labels, i);
     processed_data = append(processed_data, preprocessed_data(:,index));
 end
 
