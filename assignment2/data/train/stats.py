a605110bfc6399221aba806cbae6baba9b550461
Hella Haanstra <hellahaanstra@gmail.com>
2017-05-27 12:50:51 +0200

    Crime scene investigation

--- assignment2/data/train/stats.py
+++ assignment2/data/train/stats.py
@@ -7,11 +7,19 @@ def get_stats(data_matrix):
     stats = np.empty([5, no_variables])
     length = len(data_matrix)
     for i in range(no_variables):
+        # unique
         stats[0,i] = len(np.unique(data_matrix[~np.isnan(data_matrix[:,i])]))
+
+        # missing
         stats[1,i] = data_matrix[:,i].size - np.count_nonzero(~np.isnan(data_matrix[:,i]))
+
+        # zeros
         stats[2,i] = length - np.count_nonzero(data_matrix[~np.isnan(data_matrix[:,i])])
+
+        # set al zeros in this column to NaN for more easy calculation
         data_matrix[data_matrix[:,i] == 0] = np.nan
 
+        # mean and standard deviation
         stats[3,i] = np.nanmean(data_matrix[:, i])
         stats[4,i] = np.nanstd(data_matrix[:,i])
 
@@ -45,10 +53,10 @@ if __name__ == "__main__":
 
     stats[3:,:] = np.divide(stats[3:,:], 10)
 
-    # np.savetxt('unique.txt', stats[0,:])
-    # np.savetxt('missing.txt', stats[1,:])
-    # np.savetxt('zeros.txt', stats[2,:])
-    # np.savetxt('mean.txt', stats[3,:])
-    # np.savetxt('std.txt', stats[4,:])
-    np.savetxt('percentages_new.txt', np.divide(stats, total_length))
+    np.savetxt('unique01.txt', stats[0,:])
+    np.savetxt('missing01.txt', stats[1,:])
+    np.savetxt('zeros01.txt', stats[2,:])
+    np.savetxt('mean01.txt', stats[3,:])
+    np.savetxt('std01.txt', stats[4,:])
+    np.savetxt('percentages01.txt', np.divide(stats[1:3,:], total_length))
     print('Total number of entries: {}'.format(total_length))
