fbc9ead9640b8ce34b63a4c300a180b1b1eac816
Wouter Vrielink <wouter-vrielink@hotmail.com>
2017-05-27 11:10:15 +0200

    dit werkt als het goed is al

--- assignment2/processData.m
+++ assignment2/processData.m
@@ -4,9 +4,9 @@ function processed_data = processData(data)
 for i = 1:size(data, 1)
     ind = find(strcmp(data(i,:), 'NULL'));
     
-    % for now, replace all NULLs by zeros.
+    % for now, replace all NULLs by minus 1.
     for k = ind
-        data{i,k} = '0';
+        data{i,k} = '-1';
     end
     
 end
@@ -15,13 +15,20 @@ end
 labels = data(1,:);
 data(1,:) = [];
 
-% remove date
+% % TODO: omzetten data, dit doet: remove date
 data(:,2) = [];
 
-processed_data = str2double(data);
+preprocessed_data = str2double(data);
 
-
+processed_data = [];
 % TODO: add Wouters code that converts data to useful variables
 
+% This order:
+varlist = ["prop_starrating", "prop_review_score", "prop_brand_bool", "prop_location_score1", "prop_location_score2", "promotion_flag", "srch_length_of_stay", "srch_booking_window", "srch_query_affinity_score", "orig_destination_distance"];
+
+for i = varlist
+    index = find(label, i);
+    processed_data = append(processed_data, preprocessed_data(:,index));
+end
 
 end
